0b8ca0ca940605679ab34b83bbfd527fffdc56b8
tom76kimo <tom76kimo@verizonmedia.com>
2021-06-23 17:14:27 +0800

    3c51f58

---
+++ _nuxt/static/1624439641/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-test\u002F_nuxt\u002Fstatic\u002F1624439641",layout:"default",error:a,state:{articles:{list:[],currentIndex:0},editor:{isOnEditing:false}},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fnuxt-test\u002F",assetsPath:"\u002Fnuxt-test\u002F_nuxt\u002F",cdnURL:a}}}}(null));
